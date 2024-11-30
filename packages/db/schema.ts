a93ae4d53f8c7b1f6d45bda43f10817001cebf1c
Cameron McEvenue <mcevenc@gmail.com>
2024-11-29 21:35:16 -0500

    stuff

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,22 +1,24 @@
-import { sql } from 'drizzle-orm';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+
 import {
    integer,
    sqliteTable,
    text,
    uniqueIndex,
 } from 'drizzle-orm/sqlite-core';
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
 
-export const regions = sqliteTable('regions', {
-   id: integer().primaryKey({
-      autoIncrement: true,
-   }),
-   name: text().notNull(),
-   color: text().notNull(),
-});
+/*
+const regions = z.object({
+    id: z.number(),
+    name: z.string(),
+    color: z.string(),
+})
 
 export const insertRegionSchema = createInsertSchema(regions);
 export const selectRegionSchema = createSelectSchema(regions);
+
+*/
 
 export const neighbourhoods = sqliteTable('neighbourhoods', {
    id: integer().primaryKey({
@@ -25,13 +27,14 @@ export const neighbourhoods = sqliteTable('neighbourhoods', {
    nameCode: text('name_code').notNull(),
    namePretty: text('name_pretty').notNull(),
    polygonData: text('polygon_data').notNull(),
-   regionId: integer('region_id').references(() => regions.id).notNull(),
-}, (table) => {
+   //regionId: integer("region_id").references(() => regions.id).notNull(),
+}); /*, (table) => {
    return {
-      idxNameCode: uniqueIndex('idx_name_code').on(table.nameCode),
+      idxNameCode: uniqueIndex("idx_name_code").on(table.nameCode),
    };
-});
+});*/
 
+type test = z.infer<typeof selectNeighbourhoodSchema>;
 export const insertNeighbourhoodSchema = createInsertSchema(neighbourhoods);
 export const selectNeighbourhoodSchema = createSelectSchema(neighbourhoods);
 
@@ -46,12 +49,12 @@ export const pdfs = sqliteTable('pdfs', {
       neighbourhoods.id
    ).notNull(),
    url: text().notNull(),
-}, (table) => {
+}); /*, (table) => {
    return {
-      idxYearMonthNeighbourhood: uniqueIndex('idx_year_month_neighbourhood').on(
+      idxYearMonthNeighbourhood: uniqueIndex("idx_year_month_neighbourhood").on(
          table.year,
          table.monthNumber,
          table.neighbourhoodId,
       ),
    };
-});
+});*/
