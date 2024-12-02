9c57bbbedfcedabe9a7f97681e001182eb2ffe36
Cameron McEvenue <mcevenc@gmail.com>
2024-12-02 09:43:30 -0500

    ref: clearing out deno related items and data that was moved to mmr-cli repo

--- packages/cli/bin.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env deno
-import pkg from "./deno.json" with { type: "json" };
-import { program } from "commander";
-import { upload } from "./commands/upload.ts";
-import { download } from "./commands/download.ts";
-
-program.name(pkg.name).version(pkg.version, "-v, --version");
-program.addCommand(upload).addCommand(download);
-program.parse();
